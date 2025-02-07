ddb05967331ed313db5432b46ca430f6ff7b4f25
卢嘉琦 <lujiaqi@MacBook-Air.local>
2025-02-07 19:54:39 +0800

    update 250207_01

--- api/v1/collectrule_types.go
+++ api/v1/collectrule_types.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1
 
 import (
+	"regexp"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -29,18 +31,43 @@ type CollectRuleSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of CollectRule. Edit collectrule_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	LogBackend        string                `json:"logBackend"`         // 指定的日志后端
+	TargetNamespace   string                `json:"targetNamespace"`    // 过滤的ns
+	Selector          *metav1.LabelSelector `json:"selector,omitempty"` // 过滤pod的标签
+	CollectPattern    string                `json:"collectPattern"`     // 配置的采集正则表达式
+	CollectPatternReg *regexp.Regexp        `json:"-" yaml:"-" `        //给内部用的采集正则表达式
+	LogAlert          *LogAlertObj          `json:"logAlert"`           // 日志告警的配置
 }
+
+type LogAlertObj struct {
+	Url               string         `json:"url"`
+	KeyWordPattern    string         `json:"keyWordPattern"` // 配置的告警正则表达式
+	KeyWordPatternReg *regexp.Regexp `json:"-" yaml:"-" `    //给内部用的告警正则表达式
+}
+
+type CollectRulePhase string
+
+const (
+	StatusCollectRulePending    CollectRulePhase = "Pending"   // 等待日志采集中
+	StatusCollectRuleCollecting CollectRulePhase = "Collected" // 采集到日志了
+	StatusCollectRuleDeleting   CollectRulePhase = "Deleting"
+)
 
 // CollectRuleStatus defines the observed state of CollectRule
 type CollectRuleStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	SyncPhase      CollectRulePhase `json:"syncPhase"`      // 定义同步状态
+	LastDeployTime *metav1.Time     `json:"lastDeployTime"` // metav1.Time 定义时间
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.syncPhase`
+// +kubebuilder:printcolumn:name="LastDeployTime",type="date",JSONPath=".status.lastDeployTime"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:shortName=cr,scope=Cluster
 // CollectRule is the Schema for the collectrules API
 type CollectRule struct {
 	metav1.TypeMeta   `json:",inline"`
