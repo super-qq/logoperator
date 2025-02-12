8c69c4112a2783aaf5f3bc40558dcaef3acb471d
卢嘉琦 <lujiaqi@MacBook-Air.local>
2025-02-12 19:46:34 +0800

    update 250212_01

--- api/v1/collectrule_webhook.go
+++ api/v1/collectrule_webhook.go
@@ -17,7 +17,13 @@ limitations under the License.
 package v1
 
 import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -53,6 +59,90 @@ var _ webhook.Validator = &CollectRule{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *CollectRule) ValidateCreate() error {
 	collectrulelog.Info("validate create", "name", r.Name)
+
+	/*
+
+		apiVersion: logoperator.ning1875.io/v1
+		kind: CollectRule
+		metadata:
+		  name: cr-01
+		spec:
+		  logBackend: lb-1
+		  targetNamespace: default # 过滤namespace
+		  selector:
+		    app: gin-web-log # 过滤pod
+		  collectPattern: ".*xiaoyi.*"   # 过滤日志的正则
+		  logAlert:
+		    url: https://oapi.dingtalk.com/robot/send?access_token=75f08bf6f2fa40d45bc987608fa3ffa860bc9d8e2cd2b6099a5cc644ba0b3c50
+		    keyWordPattern: ".*400.*"
+		    atMobiles:
+		      - "15810947075"
+	*/
+	// 验证的点01  collectPattern 正则是否无法编译
+	// 验证的点02  logAlert 不为空，则要求url和keyWordPattern存在，但atMobiles可以为空
+	// 验证的点03  logAlert 不为空，  keyWordPattern正则是否无法编译
+	// 验证的点04  logAlert 不为空，要求url 可以被rl.ParseRequestURI检查
+	// 验证的点05  logAlert 不为空，atMobiles  手机号是否有问题
+
+	// 验证的点01  collectPattern 正则是否无法编译
+	if r.Spec.CollectPattern != "" {
+		_, err := regexp.Compile(r.Spec.CollectPattern)
+		if err != nil {
+			klog.Errorf("[CollectPattern.regexp.Compile.err][CollectRule:%v][err:%v]", r, err)
+			return err
+		}
+
+	}
+
+	if r.Spec.LogAlert != nil {
+		// 验证的点02  logAlert 不为空，则要求url和keyWordPattern存在，但atMobiles可以为空
+		if r.Spec.LogAlert.KeyWordPattern == "" {
+			err := fmt.Errorf("LogAlert.need.KeyWordPattern")
+			klog.Errorf("[CollectPattern.LogAlert.need.KeyWordPattern.err][CollectRule:%v][err:%v]", r, err)
+			return err
+		}
+		if r.Spec.LogAlert.Url == "" {
+			err := fmt.Errorf("LogAlert.need.Url")
+			klog.Errorf("[CollectPattern.LogAlert.need.Url.err][CollectRule:%v][err:%v]", r, err)
+			return err
+		}
+		if strings.HasPrefix(r.Spec.LogAlert.Url, "http") {
+			err := fmt.Errorf("LogAlert.start.http")
+
+			klog.Errorf("[CollectPattern.LogAlert.need.Url.err][CollectRule:%v][err:%v]", r, err)
+			return err
+		}
+		// 验证的点04  logAlert 不为空，要求url 可以被rl.ParseRequestURI检查
+		_, err := url.ParseRequestURI(r.Spec.LogAlert.Url)
+		if err != nil {
+			klog.Errorf("[CollectPattern.LogAlert.Url.invalidate][CollectRule:%v][err:%v]", r, err)
+			return err
+		}
+
+		//mobile verify 验证手机号
+		verifyMobileFormat := func(mobileNum string) bool {
+			regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+
+			reg := regexp.MustCompile(regular)
+			return reg.MatchString(mobileNum)
+
+		}
+
+		// 验证的点05  logAlert 不为空，atMobiles  手机号是否有问题
+		for _, m := range r.Spec.LogAlert.AtMobiles {
+			m := m
+			if !verifyMobileFormat(m) {
+				err := fmt.Errorf("AtMobiles.invalidate")
+				klog.Errorf("[CollectPattern.LogAlert.AtMobiles.invalidate.err][CollectRule:%v][err:%v]", r, err)
+				return err
+			}
+
+		}
+	}
+	klog.Infof("[CollectPattern.LogAlert.success][CollectRule:%v]", r)
+	//if instance.Spec.LogAlert != nil {
+	//	scr.KeyWordPatternReg, _ = regexp.Compile(instance.Spec.LogAlert.KeyWordPattern)
+	//}
 
 	// TODO(user): fill in your validation logic upon object creation.
 	return nil
